b2295255df853adeb3b87dfac8f73b2d1a174d04
pajsimbulan <pajsimbulan@gmail.com>
2023-02-08 10:32:52 -0800

    updated backend error codes

--- server/routes/authentication.js
+++ server/routes/authentication.js
@@ -9,7 +9,7 @@ exports.register = async (req, res) => {
     try {
         const {firstName, lastName, email, password} = req.body;
         if( (await userdb.findOne({email: email}))  != null) {
-          res.status(401).send("Account already exist");
+          res.status(403).send("Account already exist");
           return;
         }
         const salt = await bcrypt.genSalt();
@@ -33,11 +33,11 @@ exports.login = async (req, res) => {
     const {email, password} = req.body;
     const tempUser = await userdb.findOne({email: email});
     if(tempUser == null) {
-      res.status(401).send("Account doesn't exist");
+      res.status(404).send("Account doesn't exist");
       return;
     }
     if(! (await bcrypt.compare(password, tempUser.password))) {
-      res.status(401).send("Invalid Credential");
+      res.status(400).send("Invalid Credential");
       return;
     }
     
@@ -58,13 +58,13 @@ exports.check = (req, res, next) => {
     const token = authHeader.split(' ')[1];
     jwt.verify(token, jwtSecretKey, (err, user) => {
       if (err) {
-        return res.sendStatus(403);
+        return res.sendStatus(400);
       }
       req.user = user;
       next();
     });
   } else {
-    res.sendStatus(401);
+    res.sendStatus(400);
   }
 };
 
@@ -79,6 +79,6 @@ exports.authorize = (req,res) => {
       res.status(200).send(user);
     });
   } else {
-    res.sendStatus(401);
+    res.sendStatus(403);
   }
 }
