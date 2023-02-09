1c7594d6c86c4c60954628995ad8882cfc576d9b
pajsimbulan <pajsimbulan@gmail.com>
2023-02-08 21:35:32 -0800

    added alerts, fixed error handlings for signup page

---
+++ client/src/components/ErrorAlert.jsx
@@ -0,0 +1,33 @@
+import * as React from "react";
+import Alert from "@mui/material/Alert";
+import IconButton from "@mui/material/IconButton";
+import Collapse from "@mui/material/Collapse";
+import CloseIcon from "@mui/icons-material/Close";
+
+export default function SuccessActionAlert({openAlert, message, closeAlert}) {
+    const [open, setOpen] = React.useState(openAlert);
+    React.useEffect(() => {setOpen(openAlert)},[openAlert]);
+  return (
+    <Collapse in={open}>
+        <Alert
+          severity="error"
+          action={
+            <IconButton
+              aria-label="close"
+              color="inherit"
+              size="small"
+              onClick={() => {
+                setOpen(false);
+                closeAlert();
+              }}
+            >
+              <CloseIcon fontSize="inherit" />
+            </IconButton>
+          }
+          sx={{ mb: 2 }}
+        >
+          {message}
+        </Alert>
+      </Collapse>
+  );
+}
