3fa0cb83f339b5188b0d84ee531235b592e2c5ea
pajsimbulan <pajsimbulan@gmail.com>
2023-03-16 17:24:39 -0700

    changed backend vars

--- backend/server.js
+++ backend/server.js
@@ -3,27 +3,32 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const auth = require('./routes/authentication');
 const route = require('./routes/route');
-const PORT = 4000;
 const app = express();
+
+//SERVER CONSTANTS
+const PORT = 4000;
 const DB_URL = 'mongodb+srv://mailman:mailman98@cluster0.s2lr8pa.mongodb.net/?retryWrites=true&w=majority';
+const VERSION = 'v0';
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cors());
 
+//SERVER MIDDLEWARE + ENDPOINTS
+app.get(`/${VERSION}/authorize`,   auth.authorize);
+app.get(`/${VERSION}/email`,       auth.check, route.getAllEmails);
+app.get(`/${VERSION}/email/:id`,   auth.check, route.getEmail);
 
-app.get('/v0/authorize',   auth.authorize);
-app.get('/v0/email',       auth.check, route.getAllEmails);
-app.get('/v0/email/:id',   auth.check, route.getEmail);
+app.post(`/${VERSION}/user`,         auth.check, route.getUserInfo);
+app.post(`/${VERSION}/register`,   auth.register);
+app.post(`/${VERSION}/login`,      auth.login);
+app.post(`/${VERSION}/email`,      auth.check, route.sendEmail);
 
-app.post('/v0/user',         auth.check, route.getUserInfo);
-app.post('/v0/register',   auth.register);
-app.post('/v0/login',      auth.login);
-app.post('/v0/email',      auth.check, route.sendEmail);
+app.put(`/${VERSION}/user`,   auth.check, route.updateUserInfo); 
+app.put(`/${VERSION}/changepassword`, auth.changePassword);
 
-app.put('/v0/user',   auth.check, route.updateUserInfo); 
-app.put('/v0/changepassword', auth.changePassword);
 
+//SERVER and DB initialize connections
 mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => {
         console.log("Connection to Database is successful");
